ac6f5550e921a7482ec7c60313f2792f48e99c89
administrator <administrator@Dev5>
2014-07-21 14:00:13 +0530

    test commit

--- util.php
+++ util.php
@@ -51,14 +51,14 @@ date_default_timezone_set("Asia/Bangkok");
 <script type="text/javascript">
     // alert(screen.width);
 //alert(screen.height);
-    if (((screen.width <= 800) && (screen.height <= 1000)) || ((screen.width <= 1000) && (screen.height <= 800))) {
+    if (((screen.width >= 800) && (screen.height >= 1000)) || ((screen.width >= 1000) && (screen.height >= 750))) {
         // document.getElementsByClassName(".button11").style.margin-top = "35%";
 
         // alert(screen.width);
         // alert(screen.height);   
 
 
-        // window.location = "http://m.mdccorp.in/<?php echo basename($_SERVER['PHP_SELF']); ?>";
+         window.location = "http://mdccorp.in/<?php echo basename($_SERVER['PHP_SELF']); ?>";
     }
 
 </script> 
